06aaf0cf6ae940029142162307b271c557553c67
Sindhu <sindhusha.vf12@gmail.com>
2020-08-14 21:18:54 -0500

    Adding project files

---
+++ src/main/java/com/application/coronatracker/CoronaTrackerApplication.java
@@ -0,0 +1,17 @@
+package com.application.coronatracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class CoronaTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaTrackerApplication.class, args);
+		
+	
+	}
+
+}
---
+++ src/main/java/com/application/coronatracker/services/package-info.java
@@ -0,0 +1,1 @@
+package com.application.coronatracker.services;
